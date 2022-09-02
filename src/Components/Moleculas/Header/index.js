30a0675979e30c7a9401c6a18abbbda88ad43744
Adilet <workadilet@gmail.com>
2022-09-02 22:19:22 +0600

    fixed ui

--- src/Components/Moleculas/Header/index.js
+++ src/Components/Moleculas/Header/index.js
@@ -7,63 +7,16 @@ import personIcon from "../../../assets/icons/user.svg";
 import { useDispatch, useSelector } from "react-redux";
 
 const Header = () => {
-    return (
-        <header className={styles.wrapper}>
-            <div className="wrapper">
-                <div className={styles.content}>
-                    <div className={styles.logoBlock}>
-                        <div>
-                            <img src={logo} alt="logo"/>
-                        </div>
-                        <h1>
-                            MARMOSET <br/> TOKEN
-                        </h1>
-                    </div>
-                    <nav className={styles.navigation}>
-                        <Link
-                            to="/"
-                            className={styles.link}
-                        >
-                            Home
-                        </Link>
-                        <Link
-                            className={styles.link}
-                            to="/us"
-                        >
-                            About us
-                        </Link>
-                        <Link
-                            className={styles.active}
-                            to="/lottery"
-                        >
-                            Lottery
-                        </Link>
-                        <Link
-                            className={styles.active}
-                            to="/shop"
-                        >
-                            Shop MMT
-                        </Link>
-                        <Link
-                            className={styles.link}
-                            to="/paper"
-                        >
-                            White Paper
-                        </Link>
-                        <Link
-                            className={styles.link}
-                            to="/faq"
-                        >
-                            FAQ
-                        </Link>
-                    </nav>
-                    <div className={styles.btns}>
-                        <img src={personIcon} alt="image icon"/>
-                        <WalletBtn balance={"0.000"}/>
-                        <li className={styles.changeLang}>RU</li>
-                    </div>
-                </div>
-
+  const balance = useSelector((state) => state.walletInfo);
+  const dispatch = useDispatch();
+  console.log("balance info", balance);
+  return (
+    <header className={styles.wrapper}>
+      <div className="wrapper">
+        <div className={styles.content}>
+          <div className={styles.logoBlock}>
+            <div>
+              <img src={logo} alt="logo" />
             </div>
             <h1>
               MARMOSET <br /> TOKEN
@@ -122,10 +75,7 @@ const Header = () => {
           <div className={styles.btns}>
             <img src={personIcon} alt="image icon" />
             <WalletBtn
-              onClick={() => {
-                dispatch();
-              }}
-              balance={balance.balance}
+              
             />
             <li className={styles.changeLang}>RU</li>
           </div>
