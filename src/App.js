de88b309c19542aad2a3cb8b7a48201827ac8999
Abdurasulov Maksat <maxabdurasulov@gmail.com>
2022-09-19 15:46:16 +0600

    shop page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import "./App.css";
-import { Route, Routes, useLocation } from "react-router-dom";
+import {Route, Routes, useLocation} from "react-router-dom";
 import Header from "./Components/Moleculas/Header";
 import Main from "./Pages/MainPage";
 import Footer from "./Components/Moleculas/Footer";
 import Lottery from "./Pages/Lottery";
-import { useSelector, useDispatch } from "react-redux";
+import Shop from "./Pages/Shop";
 
 function App() {
-  const location = useLocation();
-  useEffect(() => {
-    window.scrollTo({
-      behavior: "smooth",
-      top: "0",
-    });
-  }, [location.pathname]);
-  return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route element={<Main />} path="/" />
-        <Route element={<Lottery />} path="/lottery" />
-      </Routes>
-      <Footer />
-    </div>
-  );
+    const location = useLocation();
+    useEffect(() => {
+        window.scrollTo({
+            behavior: "smooth",
+            top: "0",
+        });
+    }, [location.pathname]);
+    return (
+        <div className="App">
+            <Header/>
+            <Routes>
+                <Route element={<Main/>} path="/"/>
+                <Route element={<Lottery/>} path="/lottery"/>
+                <Route element={<Shop/>} path="/shop"/>
+            </Routes>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Pages/Shop/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from './shop.module.scss';
+import Gif from '../../assets/gif/GIF_1920х10800001-0239.mp4';
+import ShopForm from "../../Components/Organism/ShopForm";
+import rezak from "../../assets/icons/rezak.png";
+
+const Shop = () => {
+    return (
+        <div className={styles.shop}>
+            <div className={styles.video}>
+                <video loop={true} muted={true} controls={false} autoPlay={true}>
+                    <source src={Gif} type="video/mp4"/>
+                </video>
+            </div>
+            <div className={styles.box}>
+                <p className={styles.description}>Тотем мармозетка - символизирует мудрость, храбрость, ловкость и
+                    самоотверженность, обладает
+                    чувственностью, даром находить и получать удовольствия, а так же талантом быстро ориентироваться в
+                    ситуации и умением использовать в свою пользу. Отличается подвижностью и шустростью – в мыслях и
+                    действиях, идеально подходит в качестве отличного оберега от зла. Настраиваясь на тотем Мармозетка,
+                    вы можете попросить ее усилить стороны вашей личности и развить их.
+                    <br/><br/>
+                    Во многих странах мира обладает статусом священного животного, может стать прекрасным талисманом,
+                    оберегом, тотемом для Вас и ваших близких!
+                    <br/><br/>
+                    С помощью токенов ММТ вы можете приобрести домашний тотем указав в форме данные для доставки
+                    посылки. Стоимость 99$ (по курсу токена ММТ) + бесплатная доставка по всему миру.
+                </p>
+                <ShopForm/>
+            </div>
+            <img className={styles.rezak} src={rezak} alt="" />
+        </div>
+    );
+};
+
+export default Shop;
